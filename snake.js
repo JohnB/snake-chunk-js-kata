481a4cab1ac0e92a777f14f66d6d5e536a2f00c7
Zee Spencer <zee@zeespencer.com>
2014-01-10 12:47:37 -0800

    Write function to a move a single segment snake down

--- snake.js
+++ snake.js
@@ -1,47 +1,63 @@
-// Now that we have a snake being drawn on the screen, We're going to want to
-// be able to draw it many times, for instance every time it moves.
-//
-// To do this, we'll define a function which draws the snake it's given:
-
 var drawSnake = function(snakeToDraw) {
   var drawableSnake = { color: "blue", pixels: snakeToDraw };
   var drawableObjects = [drawableSnake];
   CHUNK.draw(drawableObjects);
 }
 
-// You'll notice this is similar to how we used the `var` keyword and `=`
-// symbol to create the snake earlier. The only difference is instead of
-// assigning a list of objects to a variable, we're assigning a chunk of code
-// that we want to use later. If we were to to relay this command to Sean, it
-// would look something like this:
-//
-// "Hey Sean, I'm going to teach you how to draw a snake. When someone tells
-// you to `drawSnake` and hands you a `snakeToDraw`, you are going to create an
-// object called `drawableSnake` with a color of blue and pixels of
-// `snakeToDraw`. Create a list of `drawableObjects` and put the drawableSnake
-// in it.  Now get the value of draw from  CHUNK and hand it the
-// drawableObjects. Thanks!"
-
-// Syntax Breakdown:
-//
-// `function() { }` says "Hey, between the curly braces is some code we want to
-// be able to execute." Functions can be stored inside variables just like
-// words and numbers and objects and lists.
-//
-// `(` and `)` in this case are a way to say that this function expects
-// a value to be given to it when it is called. These values are called
-// `arguments`, and can be used inside of the function body (the code between
-// the curly braces).
-
-
-// Now all we have to do is call drawSnake with a snake and it will be drawn on
-// the screen!
-
 var snake = [{ top: 0, left: 0}];
 drawSnake(snake);
 
-// "Hey Sean? Remember `drawSnake`? Well, draw this snake!"
+// Now that we have a way to repeatedly draw the snake, we want to be able to
+// also repeatebly move the snake.
 //
-// Before you move on, Open up the console and play around with making the
-// snake move by calling `drawSnake` with snakes with different `top` and
-// `left` values for their segment.
+// To do this, we're going to write a function which takes a snake changes its
+// segments top and left values.
+
+// To make our lives easier, let's make some assumptions:
+
+// 1. The snake is only 1 segment long
+// 2. The snake always moves down
+
+// By making these assumptions it keeps the code clear and understandable, as
+// well as makes it easy to check our work.
+
+var moveSnake = function(snake) {
+  var oldSegment = snake[0];
+  var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };
+  var newSnake = [newSegment];
+  return newSnake;
+}
+
+// In human-speak: "Hey Sean, I'm teaching you how to move a snake.
+// First, Get the first segment and assign it to a variable caled oldSegment.
+// Second, create a new segment whose top is one more than the previous
+// segments top and whose left is the same as the previous segments left.
+// Third, create a new snake which is an array containing just the new segment.
+// Fourth, return the new snake to whoever asked you to move the snake."
+
+// Syntax Breakdown:
+//
+// `return value` - Tells the function to immediately respond with whatever value it
+// is given. In this case, the newSnake.
+//
+// `array[location] -"Get the value at this location in the array." Arrays in
+// most programming languages are "zero-indexed" i.e. the first element is at
+// 0, the second is at 1, and so
+// on.
+//
+// `+` adds the values on the left and right together. Surprise, eh?
+
+// Now that we have a function to move the snake, we can start calling it and
+// then calling the drawSnake function with it's result to move the snake down
+// the screen! Copy and paste the following lines into your javascript console
+// a few times:
+
+snake = moveSnake('down', snake);
+drawSnake(snake);
+
+// Before moving on:
+//
+// 1. Can you make the snake move right intead of down?
+// 2. What happens to the newSnake variable? Can you use it outside of the
+// moveSnake functions curly-braces?
+// 3. Why are we re-assigning snake a new value with the results of moveSnake?
